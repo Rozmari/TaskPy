82aeb2fe611fbb5a3f6709b20b38da39edaf3592
Rozmari <ethanmichaelfl@gmail.com>
2018-02-27 11:03:19 -0500

    Syntax & whitespace edits.

--- TaskScout.py
+++ TaskScout.py
@@ -16,16 +16,14 @@ def fileread():
     return
 
 parser = argparse.ArgumentParser(prog='TaskPy', description='TaskScout-Py. 100% betterer.')
-
 # This is the main part of the script -- The 'addition' tag. Input here should be in quotes, double or otherwise.
-parser.add_argument('-a', '--add', nargs='+',  default='',
+parser.add_argument('-a', '--add', nargs='+', default='-',
         help="The task you're hoping to add.")
-
 parser.add_argument('-v', '--version', action='version', version='%(prog)s 2.0')
 
 args=parser.parse_args()
 
 #This prints the output to a file rather than to the terminal, allowing for recall of tasks later on after a break / program end.
 print(args.add, file=open(output, "a"))
-
 fileread()
+
