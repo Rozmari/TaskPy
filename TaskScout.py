404f9a751e8c597af928a5adf47b2873841afa87
rozmari <ethanmichaelfl@gmail.com>
2018-03-12 00:59:31 -0400

    New commenting & adding functions and argparse arguments.

--- TaskScout.py
+++ TaskScout.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Here we go. TaskScout V.3, Python Edition.
+# TaskScout 3 - Cross the water -- Home through the town, past the shadows.
 
 import argparse
 import sys
+import os
+
 output = "/home/geweih/output.txt"
 
-# This is a function that opens up the output and reads it.
+# This is a function that opens up the output and reads it. // Fall down wherever we meet...
 def fileread():
     f = open(output, 'r')
     file_contents = f.read()
@@ -15,15 +17,30 @@ def fileread():
     f.close()
     return
 
-parser = argparse.ArgumentParser(prog='TaskPy', description='TaskScout-Py. 100% betterer.')
-# This is the main part of the script -- The 'addition' tag. Input here should be in quotes, double or otherwise.
-parser.add_argument('-a', '--add', nargs='+', default='-',
-        help="The task you're hoping to add.")
-parser.add_argument('-v', '--version', action='version', version='%(prog)s 2.0')
+def RememberAnna():
+    ## You're going to want to save the file in a directory here.
+    ## List the directory as a var for the sake of ease.
+    os.remove(output)
+    echo "Anna begins to change her mind. // List sent to the Boatman."
+    return
 
+def ForgetItAll():
+    os.remove(output)
+    echo "All your love was just a dream. // Just like that, it's over."
+    return
+
+parser = argparse.ArgumentParser(prog='TaskPy', description='TaskScout-Py. 100% betterer.')
+# This is the main part of the script -- The 'addition' tag. Input here should be in quotes, double or otherwise. // I'm almost drowning in her sea.
+parser.add_argument('-a', '--add', nargs='+', default='-',
+        help="The task you're adding. All input will be accepted as one line. If you want different tasks on different lines, please add them one-by-one.")
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 2.0');
+
+## parser.add_argument('-c', '--clear', action='store_const',default=act1,const=act2)
+## parser.add_argument('-cs', '--clearsave', action='store_const',default=act1,const=act2);
+## parser.add_argument('-s', '--see', action='store_const',default=act1,const=act2);
 args=parser.parse_args()
 
-#This prints the output to a file rather than to the terminal, allowing for recall of tasks later on after a break / program end.
+#This prints the output to a file rather than to the terminal, allowing for recall of tasks later on after a break / program end. // Take the way home.
 print(args.add, file=open(output, "a"))
-fileread()
+## fileread()
 
