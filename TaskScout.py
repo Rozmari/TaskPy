f2974c442c28b32bf5a7abd587d9a73baad051fe
rozmari <ethanmichaelfl@gmail.com>
2018-03-15 11:45:02 -0400

    Added a clearsave and save function, clearsave not ready yet.

--- TaskScout.py
+++ TaskScout.py
@@ -17,16 +17,18 @@ def fileread():
     f.close()
     return
 
+# Clearsave function.
 def RememberAnna():
     ## You're going to want to save the file in a directory here.
     ## List the directory as a var for the sake of ease.
     os.remove(output)
-    echo "Anna begins to change her mind. // List sent to the Boatman."
+    print("Anna begins to change her mind. // List sent to the Boatman.")
     return
 
+# Clear function.
 def ForgetItAll():
-    os.remove(output)
-    echo "All your love was just a dream. // Just like that, it's over."
+    os.remove("/home/geweih/output.txt")
+    print("All your love was just a dream. // Just like that, it's over.")
     return
 
 parser = argparse.ArgumentParser(prog='TaskPy', description='TaskScout-Py. 100% betterer.')
@@ -34,13 +36,18 @@ parser = argparse.ArgumentParser(prog='TaskPy', description='TaskScout-Py. 100% 
 parser.add_argument('-a', '--add', nargs='+', default='-',
         help="The task you're adding. All input will be accepted as one line. If you want different tasks on different lines, please add them one-by-one.")
 parser.add_argument('-v', '--version', action='version', version='%(prog)s 2.0');
+parser.add_argument('-c' ,dest="clear", action='store_const'  ,const=True)
+parser.add_argument('-cs',dest="clearsave", action='store_const',const=True)
 
-## parser.add_argument('-c', '--clear', action='store_const',default=act1,const=act2)
-## parser.add_argument('-cs', '--clearsave', action='store_const',default=act1,const=act2);
-## parser.add_argument('-s', '--see', action='store_const',default=act1,const=act2);
-args=parser.parse_args()
+results = args=parser.parse_args()
 
 #This prints the output to a file rather than to the terminal, allowing for recall of tasks later on after a break / program end. // Take the way home.
 print(args.add, file=open(output, "a"))
-## fileread()
 
+if results.clear:
+    ForgetItAll()
+else:
+    if results.clearsave:
+        RememberAnna()
+    else:
+        fileread()
